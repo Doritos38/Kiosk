907455d5980f84b746c269f7586ca4b8b257bf1a
Doritos <mydjkim@naver.com>
2025-01-14 10:36:33 +0900

    Feat: Add Lv5
    Lv5 완료
    MenuItem, Menu 필드 캡슐화

---
+++ kiosk/src/main/java/com/example/kiosk/lv5/Kiosk.java
@@ -0,0 +1,59 @@
+package com.example.kiosk.lv5;
+
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Kiosk {
+
+    public void start(List<Menu> menu){
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("[ MAIN MENU ]");
+            int countCtg = 1;
+            for (Menu menuSelect : menu) {
+                System.out.println(countCtg + ". " + menuSelect.getCategory());
+                countCtg++;
+            }
+            System.out.println("0. 종료  | 종료");
+
+            int choiceCtg = sc.nextInt();
+            sc.nextLine();
+
+            if(choiceCtg > 0 && choiceCtg <= menu.size()) {
+                int countMenu = 1;
+                while (true) {
+                    List<MenuItem> menuItems = menu.get(choiceCtg - 1).getItems();
+                    System.out.println("[ " + menu.get(choiceCtg - 1).getCategory() + " MENU ]");
+                    for (MenuItem menuItem : menuItems) {
+                        System.out.println(countMenu + ". " + menuItem.getName() + " | W " + menuItem.getPrice() + " | " + menuItem.getInfo());
+                        countMenu++;
+                    }
+                    System.out.println("0. 뒤로가기");
+
+                    int choice = sc.nextInt();
+                    sc.nextLine();
+
+                    if (choice == 0) {
+                        System.out.println("메인메뉴로 이동합니다.");
+                        break;
+                    } else if (choice <= menuItems.size() - 1 && choice > 0) {
+                        MenuItem choiceMenu = menuItems.get(choice - 1);
+                        System.out.println("선택한 메뉴: " + choiceMenu.getName() + " | W " + choiceMenu.getPrice() + " | " + choiceMenu.getInfo());
+                        countMenu = 1;
+                    } else {
+                        System.out.println("잘못된 입력입니다.");
+                        countMenu = 1;
+                    }
+                }
+            }else if(choiceCtg == 0){
+                System.out.println("프로그램이 종료됩니다.");
+                break;
+            }else{
+                System.out.println("잘못된 입력입니다.");
+            }
+        }
+    }
+
+}
---
+++ kiosk/src/main/java/com/example/kiosk/lv5/MenuItem.java
@@ -0,0 +1,25 @@
+package com.example.kiosk.lv5;
+
+public class MenuItem {
+    private final String name;
+    private final double price;
+    private final String info;
+
+    public MenuItem(String name, double price, String info){
+        this.name = name;
+        this.price = price;
+        this.info = info;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+
+    public String getInfo(){
+        return this.info;
+    }
+}
